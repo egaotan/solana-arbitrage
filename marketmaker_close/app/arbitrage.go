4b99591dfe63cf302b65dd7f413b391a2c04585c
egaotan <chain_dev@163.com>
2022-02-25 08:39:55 +0800

    update

--- marketmaker_close/app/arbitrage.go
+++ marketmaker_close/app/arbitrage.go
@@ -240,7 +240,7 @@ func (arb *Arbitrage) ArbitrageClose() error {
 	arb.nonce = arb.nonce % 200
 	{
 		data := make([]byte, 2)
-		data[0] = 60
+		data[0] = 10
 		data[1] = arb.nonce
 		instruction := &program.Instruction{
 			IsAccounts:  accounts,
@@ -249,9 +249,19 @@ func (arb *Arbitrage) ArbitrageClose() error {
 		}
 		ins = append(ins, instruction)
 	}
-	if false {
+	{
 		data := make([]byte, 1)
-		data[0] = 8
+		data[0] = 11
+		instruction := &program.Instruction{
+			IsAccounts:  accounts,
+			IsData:      data,
+			IsProgramID: program.Arbitrage,
+		}
+		ins = append(ins, instruction)
+	}
+	{
+		data := make([]byte, 1)
+		data[0] = 60
 		instruction := &program.Instruction{
 			IsAccounts:  accounts,
 			IsData:      data,
