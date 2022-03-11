d8f6d7a8b61e2fcd6cd2805339ac6104f03c26dd
egaotan <chain_dev@163.com>
2022-03-11 15:55:01 +0800

    update

--- serumv2/program.go
+++ serumv2/program.go
@@ -1260,9 +1260,8 @@ func (p *Program) ConsumeEvents(parameter map[string]interface{}) ([]*solana.Acc
 	IsAccounts := []*solana.AccountMeta{
 		{PublicKey: openOrdersKey, IsSigner: false, IsWritable: true},
 		{PublicKey: market.Key, IsSigner: false, IsWritable: true},
+		{PublicKey: market.EventQueue, IsSigner: false, IsWritable: true},
 		{PublicKey: market.Key, IsSigner: false, IsWritable: true},
-		{PublicKey: market.EventQueue, IsSigner: false, IsWritable: true},
-		{PublicKey: market.EventQueue, IsSigner: false, IsWritable: true},
 		{PublicKey: market.EventQueue, IsSigner: false, IsWritable: true},
 	}
 	return IsAccounts, nil
